659f647121a723d9940c53d20a5e2df5c84b118f
osadolortech <brightosadolor8@gmail.com>
2022-08-17 15:14:49 +0100

    update

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: python manage.py migrate
-release: python manage.py migrate
 web gunicorn twitter_clonev2.wsgi:application --log-file -
--- tweetapi/serializer.py
+++ tweetapi/serializer.py
@@ -23,12 +23,13 @@ class UserSerializer(serializers.ModelSerializer):
             "id","tweet","comments","retweets","likes","first_name","last_name","user_profile","username"
         )
 
+
 class TweetSerializer(serializers.ModelSerializer):
     comments = serializers.PrimaryKeyRelatedField(many=True,read_only=True)
     likes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     retweets = serializers.PrimaryKeyRelatedField(many=True,read_only= True)
     owner= serializers.ReadOnlyField(read_only=True, source='owner.username')
-    name = serializers.PrimaryKeyRelatedField(read_only=True, source="name.first_name")
+    name = serializers.ReadOnlyField(read_only=True, source='owner.first_name')
     class Meta:
         model = TweetModel
         fields = (
